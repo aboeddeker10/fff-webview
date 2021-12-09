f2bfff271994700bdc65a226e554b0c961b4c21b
adrian <adrianlineweaver@gmail.com>
2021-12-09 11:31:01 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,15 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { WebView } from 'react-native-webview'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends React.Component {
+  webview = null
+  render() {
+    return (
+      <WebView
+        ref={(ref) => {
+          this.webview = ref
+        }}
+        javaScriptEnabled={true}
+        style={styles.container}
+        source={{ uri: 'https://www.flyfishfinder.com/auth' }}
+        onNavigationStateChange={this.handleWebViewNavigationStateChange}
+      />
+    )
+  }
+
+  handleWebViewNavigationStateChange = (navState) => {
+    const { url } = navState
+
+    if (!url) return
+
+    if (url === 'https://blog.flyfishfinder.com/') {
+      console.log('here')
+      const newURL = 'https://www.flyfishfinder.com/auth'
+      const redirectTo = 'window.location = "' + newURL + '"'
+      this.webview.injectJavaScript(redirectTo)
+    }
+  }
 }
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
@@ -18,4 +42,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
